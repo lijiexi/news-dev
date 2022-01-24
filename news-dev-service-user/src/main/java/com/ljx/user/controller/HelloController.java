f2a6b58d298fe34cef7f23eae687c9ae3245c155
Jiexi <lijiexi@Jiexi.local>
2022-01-24 23:52:31 +0000

    Initial commit

---
+++ news-dev-service-user/src/main/java/com/ljx/user/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ljx.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object hello(){
+        return "hello world";
+    }
+}
